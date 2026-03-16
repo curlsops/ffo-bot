04839f7c1ecf2675800d744c0712d0181dcf8304
MrCurls <business@mrcurls.org>
2026-03-16 02:26:33 -0500

    fix(migration): resolve CodeQL unused global variable findings

--- database/migrations/versions/010_servers_config_jsonb.py
+++ database/migrations/versions/010_servers_config_jsonb.py
@@ -6,14 +6,13 @@ Create Date: 2026-03-10
 
 """
 
-from typing import Sequence, Union
+from typing import Union
 
 from alembic import op
 
 revision: str = "010_servers_config_jsonb"
 down_revision: Union[str, None] = "009_anonymous_post_channels"
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+__all__ = [revision, down_revision]
 
 
 def upgrade() -> None:
